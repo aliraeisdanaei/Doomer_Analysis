ab22344632d97d20ec387fbb945f19dd764847d9
aliraeisdanaei <mrrookie2@gmail.com>
2022-11-15 13:15:25 -0500

    Changes made to notebook and the scraper script

--- snscrape.sh
+++ snscrape.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-snscrape -n 100000 --progress --jsonl reddit-subreddit doomer > Data/r_doomer_scraped.json
+snscrape -n 400000 --progress --jsonl reddit-subreddit doomer > Data/r_doomer_scraped.json
+snscrape -n 400000 --progress --jsonl reddit-subreddit loneliness > Data/r_loneliness_scraped.json
+# snscrape -n 400000 --progress --jsonl reddit-subreddit depression > Data/r_depression_scraped.json
