f90c6341ba731888a203ef4693c9762237ff5730
aliraeisdanaei <mrrookie2@gmail.com>
2022-11-15 21:18:24 -0500

    Wordclouds have been produced for three subreds

--- snscrape.sh
+++ snscrape.sh
@@ -2,4 +2,4 @@
 
 snscrape -n 400000 --progress --jsonl reddit-subreddit doomer > Data/r_doomer_scraped.json
 snscrape -n 400000 --progress --jsonl reddit-subreddit loneliness > Data/r_loneliness_scraped.json
-# snscrape -n 400000 --progress --jsonl reddit-subreddit depression > Data/r_depression_scraped.json
+snscrape -n 400000 --progress --jsonl reddit-subreddit depression > Data/r_depression_scraped.json
