151eb7eec3b43c67ee4a329071e59714665e30c1
aliraeisdanaei <mrrookie2@gmail.com>
2022-11-16 21:17:17 -0500

    Added a new subreddit for loneliness

--- snscrape.sh
+++ snscrape.sh
@@ -2,4 +2,5 @@
 
 snscrape -n 400000 --progress --jsonl reddit-subreddit doomer > Data/r_doomer_scraped.json
 snscrape -n 400000 --progress --jsonl reddit-subreddit loneliness > Data/r_loneliness_scraped.json
+snscrape -n 100000 --progress --jsonl reddit-subreddit lonely > Data/r_lonely_scraped.json
 snscrape -n 400000 --progress --jsonl reddit-subreddit depression > Data/r_depression_scraped.json
